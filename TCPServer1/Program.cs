d6008aa4dce024f2748de353b717cf5796339e5c
move <move@zealand.dk>
2024-08-29 12:11:55 +0200

    Add project files.

---
+++ TCPServer1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("TCP Server 1");
+
+TcpListener listener = new TcpListener(IPAddress.Any, 7);
+
+listener.Start();
+while (true)
+{
+    TcpClient socket = listener.AcceptTcpClient();
+    IPEndPoint clientEndPoint = socket.Client.RemoteEndPoint as IPEndPoint;
+    Console.WriteLine("Client connected: " + clientEndPoint.Address);
+
+    NetworkStream ns = socket.GetStream();
+    StreamReader reader = new StreamReader(ns);
+    StreamWriter writer = new StreamWriter(ns);
+
+    while (socket.Connected)
+    {
+        string? message = reader.ReadLine();
+        Console.WriteLine(message);
+        writer.WriteLine(message);
+        writer.Flush();
+
+        if (message == "stop")
+        {
+            socket.Close();
+        }
+    }
+}
+
+listener.Stop();
+
